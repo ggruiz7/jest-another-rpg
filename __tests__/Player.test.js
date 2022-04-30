db09aac815645efd0d00d0bd06eb274fff706d41
Gabriel Ruizz <garciagabriel1945@gmail.com>
2022-04-29 17:27:17 -0700

    commiting current

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,1 +1,35 @@
-const Potion = require('../lib/Player')
+const Potion = require('../lib/Potion');
+jest.mock('../lib/Potion');
+console.log(new Potion());
+
+const Player = require('../lib/Player');
+
+test('creates a new player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
+});
+
+test("gets players stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProprerty('agilty');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
